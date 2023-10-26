e4e2c95887e58e753ebcdd657af2edb25bd7ad5a
nduhiujames <nduhiujames8648@gmail.com>
2023-10-26 14:25:58 +0300

    python code tests

--- test_my_module.py
+++ test_my_module.py
@@ -1,9 +1,13 @@
 import unittest
 from my_module import add_numbers
+from my_module import multiply_numbers
 
 class TestMyModule(unittest.TestCase):
     def test_add_numbers(self):
         result = add_numbers(2, 3)
+        self.assertEqual(result, 5)
+    def test_multiply_numbers(self):
+        result = multiply_numbers(2, 3)
         self.assertEqual(result, 5)
 
 if __name__ == '__main__':
