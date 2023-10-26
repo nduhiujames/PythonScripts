ac04fdb1d4b5fda89793be37a2ec11118b693c39
nduhiujames <nduhiujames8648@gmail.com>
2023-10-26 15:08:58 +0300

    Adding .yml file Created .circleci directory created config.yml

--- test_my_module.py
+++ test_my_module.py
@@ -8,7 +8,7 @@ class TestMyModule(unittest.TestCase):
         self.assertEqual(result, 5)
     def test_multiply_numbers(self):
         result = multiply_numbers(2, 3)
-        self.assertEqual(result, 5)
+        self.assertEqual(result, 6)
 
 if __name__ == '__main__':
     unittest.main()
