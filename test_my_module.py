6326db812534e77ba6a0938d36086d352b7e4974
nduhiujames <nduhiujames8648@gmail.com>
2023-11-02 12:42:03 +0300

    Modified the main python file to perform texts

--- test_my_module.py
+++ test_my_module.py
@@ -5,7 +5,7 @@ from my_module import multiply_numbers
 class TestMyModule(unittest.TestCase):
     def test_add_numbers(self):
         result = add_numbers(2, 3)
-        self.assertEqual(result, 6)
+        self.assertEqual(result, 5)
     def test_multiply_numbers(self):
         result = multiply_numbers(2, 3)
         self.assertEqual(result, 6)
